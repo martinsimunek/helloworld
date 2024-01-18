7b41cab43f3e287e8ba6d97e689c6219e9395eb4
Martin Šimůnek <public@e-mis.cz>
2024-01-18 20:28:23 +0100

    Testovací kód v 1. lekci
    Vyzkoušeli jsme si práci s debugováním...

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+// Zdrojový kód
+public class Main {  // pravý Alt+B { ... pravý Alt + N }
+    /**
+     * Popis metody, která následuje...
+     * @param args parametry z příkazové řádky
+     */
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Ahoj světe, jak se všichni máte?"); //  Komentáře (naše poznámky)
+        System.out.println("To je fajn, že nemusím psát v Poznámkovém bloku... ;)");
 
+        /*
+         *  Víceřádkový komentář
+         *  Když jeden řádek nestačí... ;)
+         */
+
+        // cyklus  // breakpoint
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            System.out.print("i = " + i);
+            if (i < 5) {
+                System.out.println(", ");
+            }
         }
     }
 }
+
